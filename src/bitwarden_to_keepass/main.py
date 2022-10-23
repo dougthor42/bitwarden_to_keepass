59938aababcf780c59a251cc9e3414b1488660ce
Douglas Thor <doug.thor@gmail.com>
2022-10-22 20:57:35 -0700

    Add bw_login and bw_unlock context managers

--- src/bitwarden_to_keepass/main.py
+++ src/bitwarden_to_keepass/main.py
@@ -1,8 +1,10 @@
 """
 """
 import os
+import subprocess
 from contextlib import contextmanager
 from typing import Dict
+from typing import Iterator
 
 from bitwarden_to_keepass import logger
 
@@ -32,6 +34,52 @@ def temp_env(new_vars: Dict[str, str]) -> Iterator[None]:
             os.environ.pop(key, None)  # Avoids KeyError
         # And restore the original
         os.environ.update(original_env)
+
+
+@contextmanager
+def bw_login() -> Iterator[None]:
+    """
+    Context manager that logs into Bitwarden on entry and logs out on exit.
+
+    Uses the env vars BW_CLIENTID and BW_CLIENTSECRET, as specified in the
+    Bitwarden CLI docs.
+    """
+    logger.info("Logging into Bitwarden")
+
+    try:
+        login = ["bw", "login", "--apikey"]
+        subprocess.run(login, check=True)
+        yield
+    finally:
+        logger.info("Logging out")
+        logout = ["bw", "logout"]
+        subprocess.run(logout, check=True)
+
+
+@contextmanager
+def bw_unlock(password_env: str = BW_MASTER_PASSWORD_ENV) -> Iterator[str]:
+    """
+    Context manager that unlocks a bitwarden vault and yields the session key.
+
+    Vault is locked upon exiting the CM.
+
+    Args:
+        + password_env: The name of the environment variable that stores the
+          Bitwarden master password.
+    """
+    logger.info("Unlocking vault")
+
+    try:
+        unlock = ["bw", "unlock", "--passwordenv", password_env]
+        result = subprocess.run(unlock, capture_output=True, check=True)
+        logger.info("Sucessfully unlocked vault.")
+        # There's a line like `$ export BW_SESSION="..."` - grab that.
+        session_key = grab_session_key(result.stdout)
+        yield session_key
+    finally:
+        logger.info("Locking vault")
+        lock = ["bw", "lock"]
+        subprocess.run(lock, check=True)
 
 
 def grab_session_key(text: bytes) -> str:
