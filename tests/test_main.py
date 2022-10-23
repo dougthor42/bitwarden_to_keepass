c4a6fc056a2644a41d7a4ac5e8eea4740ac356c0
Douglas Thor <doug.thor@gmail.com>
2022-10-22 20:34:18 -0700

    Add test_main.py

---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import pytest
+
+from bitwarden_to_keepass import main
+
+
+# A copy-paste from the `bw unlock` command with fake session key.
+STDOUT = """
+? Master password: [hidden]
+Your vault is now unlocked!
+
+To unlock your vault, set your session key to the `BW_SESSION` environment variable. ex:
+$ export BW_SESSION="foobar1234"
+> $env:BW_SESSION="foobar1234"
+
+You can also pass the session key to any command with the `--session` option. ex:
+$ bw list items --session foobar1234
+"""
+
+
+def test_grab_session_key():
+    want = "foobar1234"
+    got = main.grab_session_key(STDOUT)
+    assert got == want
+
+
+def test_grab_session_key_raises():
+    with pytest.raises(ValueError):
+        main.grab_session_key("foobar")
