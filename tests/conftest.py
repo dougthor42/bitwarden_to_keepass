dc3c5a327deef6b0a57a280f99d5f15b44b3d930
Douglas Thor <doug.thor@gmail.com>
2023-01-14 14:34:04 -0800

    Add a pytest fixture that creates an empty keepass file for playing with.

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import shutil
+import uuid
+
+import pytest
+
+from . import DATA_DIR
+
+TEST_FILENAME = "empty_db.kdbx"
+
+
+@pytest.fixture
+def keepass_file(tmp_path):
+    """'Create' an empty KeePass file in a temp directory."""
+    source = DATA_DIR / TEST_FILENAME
+    dest_filename = uuid.uuid4().hex + ".kdbx"
+    dest = tmp_path / dest_filename
+
+    shutil.copy(source, dest)
+
+    yield dest
